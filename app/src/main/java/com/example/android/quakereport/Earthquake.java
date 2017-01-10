847f9b9b11c184606b47af105a7fda88aeab6974
Francislainy <francislainy.campos@gmail.com>
2017-01-10 21:00:31 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,30 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by Francislainy on 10/01/2017.
+ */
+
+public class Earthquake {
+
+    private String magnitude;
+    private String location;
+    private String date;
+
+    public Earthquake(String magnitude, String location, String date) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.date = date;
+    }
+
+    public String getMagnitude() {
+        return magnitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
